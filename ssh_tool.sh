d684fa814f39ff511bf95167202720c14524cade
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-08-18 17:12:06 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -7,6 +7,10 @@ echo "3.一键修改root用户密码"
 echo "4.退出"
 read -p "请选择功能: " mu
 
+
+if [[ $EUID -ne 0 ]]; then
+    su='sudo'
+fi
 # 函数：检查并重启 SSH 服务
 restart_ssh_service() {
     if command -v systemctl >/dev/null 2>&1; then
@@ -37,10 +41,12 @@ restart_ssh_service() {
 }
 
 ssh_key(){
+if [[ $EUID -ne 0 ]]; then
+    echo "请以 root 用户运行此脚本，或者先执行sudo -i进入root账户后再执行脚本"
+    exit 1
+fi
 # 读取用户输入的公钥
 read -p "请输入 root 用户的公钥内容: " ssh_key
-#sudo 提权root用户身份运行
-sudo -i
 # 备份当前的 sshd_config 文件
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 # 修改 sshd_config 文件
@@ -74,14 +80,14 @@ sudo -i
 # 备份当前的 sshd_config 文件
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 # 修改 sshd_config 文件
-sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sed -i 's/^PermitRootLogin .*$/PermitRootLogin without-password/' /etc/ssh/sshd_config
+$su sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+$su sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+$su sed -i 's/^PermitRootLogin .*$/PermitRootLogin without-password/' /etc/ssh/sshd_config
 # 确保用户的 .ssh 目录和 authorized_keys 文件存在
-mkdir -p /home/$current_user/.ssh
-chmod 700 /home/$current_user/.ssh
-touch /home/$current_user/.ssh/authorized_keys
-chmod 600 /home/$current_user/.ssh/authorized_keys
+$su mkdir -p /home/$current_user/.ssh
+$su chmod 700 /home/$current_user/.ssh
+$su touch /home/$current_user/.ssh/authorized_keys
+$su chmod 600 /home/$current_user/.ssh/authorized_keys
 # 将用户的公钥添加到 authorized_keys 文件中
 echo "$ssh_key" >> /home/$current_user/.ssh/authorized_keys
 # 尝试重启 SSH 服务
@@ -98,8 +104,6 @@ rm -f $0
 root_pwd(){
 #自定义root密码
 read -p "自定义root密码: " mima
-#sudo 提权root用户身份运行
-sudo -i
 # 文件路径
 files=("/etc/passwd" "/etc/shadow")
 # 保存原始属性
@@ -109,11 +113,8 @@ for file in "${files[@]}"; do
     original_attrs[$file]=$(lsattr "$file")
 done
 # 移除文件属性（如果有的话）
-chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
-chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
-# 修改配置文件
-sed -i 's/^PasswordAuthentication .*$/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^PermitRootLogin .*$/PermitRootLogin yes/' /etc/ssh/sshd_config
+$su chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
+$su chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
 # 检查 SSH 配置文件
 prl=$(grep PermitRootLogin /etc/ssh/sshd_config)
 pa=$(grep PasswordAuthentication /etc/ssh/sshd_config)
@@ -121,8 +122,8 @@ pa=$(grep PasswordAuthentication /etc/ssh/sshd_config)
 if [[ -n $prl && -n $pa ]]; then
     if [[ -n $mima ]]; then
         echo "root:$mima" | chpasswd
-        sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-        sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+        $su sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+        $su sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
         if restart_ssh_service; then
             echo "SSH 服务已成功重启。"
             echo "VPS当前用户名：root"
